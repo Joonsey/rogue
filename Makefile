8b94e4e04138895cbb9d481c532f44020f34c3cc
Johannes <johannesr@viken.no>
2023-04-13 16:40:11 +0200

    updated build to use g++ and improved setup

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 
 main:
-	gcc src/main.cpp -o main -lglfw -lGL -lX11 -lpthread -lXrandr -lXi -ldl
+	g++ src/* -o main -lglfw -lGL -lX11 -lpthread -lXrandr -lXi -ldl
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,31 @@
 #include "../include/glad/glad.h"
 #include <GLFW/glfw3.h>
+#include <stdexcept>
+
+#define WIDTH 800
+#define HEIGHT 600
 
  GLFWwindow* create_window()
 {
-	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", NULL, NULL);
 	if (window == NULL)
 	{
 		glfwTerminate();
+		throw std::runtime_error("failed creating window");
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		throw std::runtime_error("failed loading gladloader");
 	}
 	return window;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
 }
 
 int main()
@@ -17,8 +34,16 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	auto window = create_window();
-	glfwMakeContextCurrent(window);
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	while(!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
     return 0;
 }
